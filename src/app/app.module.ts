811e5a0f8ce3253ad1b48ce590a3de7a0cb5347a
moussa Doumbia <moussa.doumbia9219@gmail.com>
2020-03-20 22:40:18 +0100

    add multi step form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ProductComponent } from './product/product.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,17 +7,20 @@ import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
 import { DeleteProductModalComponent } from './delete-product-modal/delete-product-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    DeleteProductModalComponent
+    DeleteProductModalComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
+
+@Component({
+  selector: "in-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"]
+})
+export class ProductComponent implements OnInit {
+  productForm: FormGroup;
+  @Input() product;
+
+  deviceType = "tablet";
+
+  deviceTypes = [
+    {
+      name: "Tablet",
+      icon: "tablet"
+    },
+    {
+      name: "Laptop",
+      icon: "computer"
+    },
+    {
+      name: "Phone",
+      icon: "mobile"
+    },
+    {
+      name: "Monitor",
+      icon: "display"
+    }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      basic: fb.group({
+        name: "",
+        description: "",
+        active: false,
+        features: fb.array([fb.control("")])
+      }),
+      expiration: fb.group({
+        expirationDate: null
+      })
+    });
+  }
+
+  ngOnInit() {}
+
+  selectDevice(device) {
+    this.deviceType = device.icon;
+  }
+
+  get basicFeatures(): FormArray{
+    return this.productForm.get('basic.features') as FormArray;
+  }
+
+  addFeature() {
+    this.basicFeatures.push(this.fb.control(''));
+  }
+}
